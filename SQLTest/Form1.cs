af438d3b6049e2f8637c2ade40dbd228cb6da159
walsh441 <128089783+walsh441@users.noreply.github.com>
2026-01-21 05:48:28 +0100

    Update README.md

--- SQLTest/Form1.cs
+++ SQLTest/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SQLTest
@@ -15,6 +8,31 @@ namespace SQLTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        //    SqlConnection con = new SqlConnection("Data Source=DESKTOP-JV7URL6;Initial Catalog=COCAhenkroApp;Integrated Security=True");
+        //    private void btnSave_Click(object sender, EventArgs e)
+        //    {
+
+        //        con.Open();Bmand("insert into EdificationMembers values('" + txtName.Text + "','" + cmbPosition.Text + "','" + txtContact.Text + "',getdate())", con);
+        //        command.ExecuteNonQuery();
+        //        MessageBox.Show("Successfully Saved");
+        //        con.Close();
+        //        BindData();
+        //    }
+        //    void BindData()
+        //    {
+        //        SqlCommand command = new SqlCommand("select * from EdificationMembers", con);
+        //        SqlDataAdapter sda = new SqlDataAdapter(command);
+        //        DataTable dt = new DataTable();
+        //        sda.Fill(dt);
+        //        EdificationMembersdataGridView.DataSource = dt;
+
+
+        //    }
         }
     }
 }
